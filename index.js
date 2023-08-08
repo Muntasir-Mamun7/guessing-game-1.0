b2e7dd0f5e7e576eea09e48407f75a79e574d59e
Muntasir <munmamun9@gmail.com>
2023-08-08 14:50:16 +0800

    new

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 let guessBtn = document.getElementById('guessBtn')
 let feedback = document.getElementById('feedback')
+let playAgainBtn = document.getElementById('playAgainBtn')
+let guessInput = document.getElementById('guessInput')
+let number = Math.floor(Math.random() * 100) + 1
 
-let number = [Math.floor(Math.random() * 100) + 1]
 
-guessBtn.addEventListener('click',function() {
+guessBtn.addEventListener('click', function() {
+    let guess = parseInt(guessInput.value)
 
-    let guessInput = document.getElementById('guessInput').value;
-
-    if (guessInput == number) {
-        feedback.innerHTML = `You guessed right!`
-    }
-
-    else if (guessInput < number) {
-        feedback.innerHTML =  'You guessed too low!'
-    };
-
-    if (guessInput > number) {
+    if (guess === number) {
+        feedback.innerHTML = 'You guessed right!'
+        playAgainBtn.style.display = 'block'
+    } else if (guess < number) {
+        feedback.innerHTML = 'You guessed too low!'
+    } else {
         feedback.innerHTML = 'You guessed too high!'
-}
+    }
+});
 
-   
+playAgainBtn.addEventListener('click', function() {
+    playAgainBtn.style.display = 'none'
+    number = Math.floor(Math.random() * 100) + 1;
+    feedback.innerHTML = ''
+    guessInput.value = ''
+});
+
+window.addEventListener('load', function() {
+    number = Math.floor(Math.random() * 100) + 1
+    feedback.innerHTML = ''
+    guessInput.value = ''
 })
